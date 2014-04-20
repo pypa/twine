20c55f6d752f7920494bbcea8d5f1a5719c97d81
Donald Stufft <donald@stufft.io>
2014-04-20 13:47:57 -0400

    Bump version

--- twine/__init__.py
+++ twine/__init__.py
@@ -23,7 +23,7 @@ __title__ = "twine"
 __summary__ = "Collection of utilities for interacting with PyPI"
 __uri__ = "https://github.com/dstufft/twine"
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 __author__ = "Donald Stufft and individual contributors"
 __email__ = "donald@stufft.io"
