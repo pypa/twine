8b8790b93ce7bf2441a6551e7d2d1c67f8b084ff
Ian Cordasco <graffatcolmingov@gmail.com>
2015-10-27 21:14:52 -0500

    Add tests to ensure version parsing does not regress

---
+++ tests/test_wheel.py
@@ -0,0 +1,30 @@
+# Copyright 2015 Ian Cordasco
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import unicode_literals
+
+from twine import wheel
+
+import pytest
+
+
+@pytest.fixture(params=[
+    'tests/fixtures/twine-1.5.0-py2.py3-none-any.whl',
+    'tests/alt-fixtures/twine-1.5.0-py2.py3-none-any.whl'
+])
+def example_wheel(request):
+    return wheel.Wheel(request.param)
+
+
+def test_version_parsing(example_wheel):
+    assert example_wheel.py_version == 'py2.py3'
