c1113da3d3ff0d1b248d80514568564a801f81ff
Ian Cordasco <graffatcolmingov@gmail.com>
2016-01-27 23:27:37 -0600

    Stop testing on py32 and fix pep8 errors

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,14 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
+# import sys
+# import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath(os.pardir))
+# sys.path.insert(0, os.path.abspath(os.pardir))
 
 import twine
 
