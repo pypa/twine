7b2957adc9d1629985c343c03643dd9644b52510
Sumana Harihareswara <sh@changeset.nyc>
2018-03-07 13:55:44 -0500

    Bump version to 1.10.0

--- twine/__init__.py
+++ twine/__init__.py
@@ -22,7 +22,7 @@ __title__ = "twine"
 __summary__ = "Collection of utilities for publishing packages on PyPI"
 __uri__ = "http://twine.readthedocs.io/"
 
-__version__ = "1.10.0rc1"
+__version__ = "1.10.0"
 
 __author__ = "Donald Stufft and individual contributors"
 __email__ = "donald@stufft.io"
