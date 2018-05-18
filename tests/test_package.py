cbcb856ab24addd3588dbc6ef0db751d668a1810
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2018-05-18 12:42:43 -0500

    Refactor hash generation for testability
    After reviewing #367 I realized that this code could be more easily
    tested if it was broken out from the PackageFile object. Since it was
    such a tiny refactor, I went ahead, refactored it and added the
    necessary tests to ensure this just works.

--- tests/test_package.py
+++ tests/test_package.py
@@ -163,3 +163,45 @@ def test_metadata_dictionary(gpg_signature):
 
     # GPG signature
     assert result.get('gpg_signature') == gpg_signature
+
+
+TWINE_1_5_0_WHEEL_HASHES = {
+    'md5': '1919f967e990bee7413e2a4bc35fd5d1',
+    'sha2': 'd86b0f33f0c7df49e888b11c43b417da5520cbdbce9f20618b1494b600061e67',
+    'blake2': (
+        'b657a4148d05bd0098c1d6d8cc4e14e766dbe93c3a5ab6723b969da27a87bac0'
+    ),
+}
+
+
+def test_hash_manager():
+    """Verify our HashManager works."""
+    filename = 'tests/fixtures/twine-1.5.0-py2.py3-none-any.whl'
+    hasher = package.HashManager(filename)
+    hasher.hash()
+    assert hasher.hexdigest() == TWINE_1_5_0_WHEEL_HASHES
+
+
+def test_fips_hash_manager(monkeypatch):
+    """Verify the behaviour if hashlib is using FIPS mode."""
+    replaced_md5 = pretend.raiser(ValueError('fipsmode'))
+    monkeypatch.setattr(package.hashlib, 'md5', replaced_md5)
+
+    filename = 'tests/fixtures/twine-1.5.0-py2.py3-none-any.whl'
+    hasher = package.HashManager(filename)
+    hasher.hash()
+    hashes = TWINE_1_5_0_WHEEL_HASHES.copy()
+    hashes['md5'] = None
+    assert hasher.hexdigest() == hashes
+
+
+def test_no_blake2_hash_manager(monkeypatch):
+    """Verify the behaviour with missing blake2."""
+    monkeypatch.setattr(package, 'blake2b', None)
+
+    filename = 'tests/fixtures/twine-1.5.0-py2.py3-none-any.whl'
+    hasher = package.HashManager(filename)
+    hasher.hash()
+    hashes = TWINE_1_5_0_WHEEL_HASHES.copy()
+    hashes['blake2'] = None
+    assert hasher.hexdigest() == hashes
