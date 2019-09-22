d9487ab3eb4b3bbb1d733e8deb1c557bbda42201
Jason R. Coombs <jaraco@jaraco.com>
2019-09-22 14:51:58 +0200

    Start up a devpi server for integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+import contextlib
 import textwrap
 
 import pytest
+import portend
+import requests
+import jaraco.envs
 
 from twine import settings
 
@@ -29,3 +33,33 @@ def make_settings(pypirc):
         return settings.Settings(**settings_kwargs)
 
     return _settings
+
+
+class DevPiEnv(jaraco.envs.ToxEnv):
+    """
+    Run devpi using tox:testenv:devpi.
+    """
+    name = 'devpi'
+
+    def create(self):
+        super().create()
+        self.port = portend.find_available_local_port()
+
+    def ready(self):
+        url = 'http://localhost:{port}'.format(port=self.port)
+        with contextlib.suppress(Exception):
+            return requests.get(url)
+
+
+@pytest.fixture(scope='session')
+def devpi_server(request, watcher_getter):
+    env = DevPiEnv()
+    env.create()
+    proc = watcher_getter(
+        name=str(env.exe('devpi-server')),
+        arguments=['--port', str(env.port)],
+        checker=env.ready,
+        # Needed for the correct execution order of finalizers
+        request=request,
+    )
+    return locals()
---
+++ tests/test_integration.py
@@ -0,0 +1,2 @@
+def test_devpi_upload(devpi_server):
+    pass
