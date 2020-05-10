0d5bcb4fb89559deb8228aadba6c9eef27010098
Brian Rutledge <brian@bhrutledge.com>
2020-05-10 09:20:55 -0400

    Add tests for arguments

--- tests/test_pep561.py
+++ tests/test_pep561.py
@@ -1,9 +1,59 @@
 import subprocess
+import textwrap
+
+import pytest
 
 
-def test_no_missing_import(tmpdir):
-    src = tmpdir / "example.py"
-    src.write("""import twine""")
+@pytest.fixture
+def example_py(tmpdir, monkeypatch):
+    monkeypatch.chdir(tmpdir)
+    return tmpdir / "example.py"
 
-    tmpdir.chdir()
-    subprocess.run(["mypy", "--strict", src], check=True)
+
+def test_no_missing_import(example_py):
+    example_py.write("""import twine""")
+
+    subprocess.run(["mypy", "--strict", example_py], check=True)
+
+
+def test_type_error_for_command(example_py):
+    example_py.write(
+        textwrap.dedent(
+            """
+            from twine.commands import check
+
+            check.check('example.pkg')
+            """
+        )
+    )
+
+    with pytest.raises(subprocess.CalledProcessError) as excinfo:
+        subprocess.run(
+            ["mypy", "--strict", example_py], check=True, stdout=subprocess.PIPE
+        )
+
+    assert b"1 error in 1 file" in excinfo.value.output
+    assert (
+        b'"check" has incompatible type "str"; expected "List[str]"'
+        in excinfo.value.output
+    )
+
+
+def test_type_error_for_class(example_py):
+    example_py.write(
+        textwrap.dedent(
+            """
+            from twine import repository
+
+            repo = repository.Repository()
+            """
+        )
+    )
+
+    with pytest.raises(subprocess.CalledProcessError) as excinfo:
+        subprocess.run(
+            ["mypy", "--strict", example_py], check=True, stdout=subprocess.PIPE
+        )
+
+    assert b"1 error in 1 file" in excinfo.value.output
+    assert b'Too few arguments for "Repository"' in excinfo.value.output
