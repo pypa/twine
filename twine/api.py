1b6999674463283aad7aa48c4fe5cab496f1bba0
Jason R. Coombs <jaraco@jaraco.com>
2020-05-31 14:03:45 -0400

    Add simple 'twine.api' that exposes each command. Fixes #194.

---
+++ twine/api.py
@@ -0,0 +1,12 @@
+import pkg_resources
+
+
+def install_commands():
+    globals().update(
+        (ep.name, ep.load())
+        for ep in pkg_resources.iter_entry_points(group='twine.registered_commands')
+    )
+
+
+install_commands()
+del install_commands
