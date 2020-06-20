979e37bec1f44a6c44a05aa574797bdf2b96cbc5
Jason R. Coombs <jaraco@jaraco.com>
2020-06-20 10:52:44 -0400

    Fade to black

--- twine/api.py
+++ twine/api.py
@@ -4,7 +4,7 @@ import pkg_resources
 def install_commands():
     globals().update(
         (ep.name, ep.load())
-        for ep in pkg_resources.iter_entry_points(group='twine.registered_commands')
+        for ep in pkg_resources.iter_entry_points(group="twine.registered_commands")
     )
 
 
