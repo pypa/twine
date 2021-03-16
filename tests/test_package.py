pypa/twine

commit 0bd26af11b8f97ca97ccb9859693d7a14acf15c3

Replace the remaining uses of pkg_resources with packaging and importlib_metadata. (#736)

twine/package.py
@@ -17,7 +17,8 @@ import os
 import subprocess
 from typing import Dict, NamedTuple, Optional, Sequence, Tuple, Union
 
-import pkg_resources
+import importlib_metadata
+import packaging.utils
 import pkginfo
 
 from twine import exceptions
@@ -58,7 +59,7 @@ class PackageFile:
         self.metadata = metadata
         self.python_version = python_version
         self.filetype = filetype
-        self.safe_name = pkg_resources.safe_name(metadata.name)
+        self.safe_name = packaging.utils.canonicalize_name(metadata.name)
         self.signed_filename = self.filename + ".asc"
         self.signed_basefilename = self.basefilename + ".asc"
         self.gpg_signature: Optional[Tuple[str, bytes]] = None
@@ -99,8 +100,10 @@ class PackageFile:
 
         py_version: Optional[str]
         if dtype == "bdist_egg":
-            pkgd = pkg_resources.Distribution.from_filename(filename)
-            py_version = pkgd.py_version
+            (dist,) = importlib_metadata.Distribution.discover(  # type: ignore[no-untyped-call] # python/importlib_metadata#288  # noqa: E501
+                path=[filename]
+            )
+            py_version = dist.metadata["Version"]
         elif dtype == "bdist_wheel":
             py_version = meta.py_version
         elif dtype == "bdist_wininst":

tests/test_package.py
@@ -264,3 +264,8 @@ def test_malformed_from_file(monkeypatch):
         package_file.PackageFile.from_filename(filename, comment=None)
 
     assert "Invalid distribution file" in err.value.args[0]
+
+
+def test_package_from_egg():
+    filename = "tests/fixtures/twine-3.3.0-py3.9.egg"
+    package_file.PackageFile.from_filename(filename, comment=None)

mypy.ini
@@ -22,6 +22,9 @@ strict_equality = True
 ; https://github.com/tartley/colorama/issues/206
 ignore_missing_imports = True
 
+[mypy-packaging]
+ignore_missing_imports = True
+
 [mypy-pkginfo]
 ; https://bugs.launchpad.net/pkginfo/+bug/1876591
 ignore_missing_imports = True
@@ -35,10 +38,6 @@ ignore_missing_imports = True
 ignore_missing_imports = True
 
 [mypy-rfc3986]
-ignore_missing_imports = True
-
-[mypy-setuptools]
-; https://github.com/python/typeshed/issues/2171
 ignore_missing_imports = True
 
 [mypy-tqdm]

setup.cfg
@@ -38,7 +38,7 @@ install_requires=
     readme_renderer >= 21.0
     requests >= 2.20
     requests-toolbelt >= 0.8.0, != 0.9.0
-    setuptools >= 0.7.0
+    packaging >= 16.2
     tqdm >= 4.14
     importlib_metadata >= 3.6
     keyring >= 15.1

tests/test_repository.py
@@ -89,7 +89,14 @@ def test_make_user_agent_string(default_repo):
     assert "User-Agent" in default_repo.session.headers
 
     user_agent = default_repo.session.headers["User-Agent"]
-    packages = ("twine/", "requests/", "requests-toolbelt/", "pkginfo/", "setuptools/")
+    packages = (
+        "twine/",
+        "requests/",
+        "requests-toolbelt/",
+        "pkginfo/",
+        "importlib_metadata/",
+        "packaging/",
+    )
     assert all(p in user_agent for p in packages)
 
 

twine/_installed.py
@@ -1,60 +1,0 @@
-# Copyright 2013 Tres Seaver
-# Copyright 2015 Ian Cordasco
-# This code was originally licensed under the Python Software Foudation
-# License by Tres Seaver. In order to facilitate finding the metadata of
-# installed packages, part of the most current implementation of the
-# pkginfo.Installed class is reproduced here with bug fixes from
-# https://bugs.launchpad.net/pkginfo/+bug/1437570.
-import glob
-import os
-import sys
-import warnings
-from typing import Optional
-
-import pkginfo
-
-
-class Installed(pkginfo.Installed):
-    def read(self) -> Optional[str]:
-        opj = os.path.join
-        if self.package is not None:
-            package = self.package.__package__
-            if package is None:
-                package = self.package.__name__
-            egg_pattern = "%s*.egg-info" % package
-            dist_pattern = "%s*.dist-info" % package
-            file: Optional[str] = getattr(self.package, "__file__", None)
-            if file is not None:
-                candidates = []
-
-                def _add_candidate(where: str) -> None:
-                    candidates.extend(glob.glob(where))
-
-                for entry in sys.path:
-                    if file.startswith(entry):
-                        _add_candidate(opj(entry, "METADATA"))  # egg?
-                        _add_candidate(opj(entry, "EGG-INFO"))  # egg?
-                        # dist-installed?
-                        _add_candidate(opj(entry, egg_pattern))
-                        _add_candidate(opj(entry, dist_pattern))
-                dir, name = os.path.split(self.package.__file__)
-                _add_candidate(opj(dir, egg_pattern))
-                _add_candidate(opj(dir, dist_pattern))
-                _add_candidate(opj(dir, "..", egg_pattern))
-                _add_candidate(opj(dir, "..", dist_pattern))
-
-                for candidate in candidates:
-                    if os.path.isdir(candidate):
-                        path = opj(candidate, "PKG-INFO")
-                        if not os.path.exists(path):
-                            path = opj(candidate, "METADATA")
-                    else:
-                        path = candidate
-                    if os.path.exists(path):
-                        with open(path) as f:
-                            return f.read()
-
-        warnings.warn(
-            "No PKG-INFO or METADATA found for package: %s" % self.package_name
-        )
-        return None

twine/cli.py
@@ -14,27 +14,24 @@
 import argparse
 from typing import Any, List, Tuple
 
-import pkginfo
-import requests
-import requests_toolbelt
-import setuptools
-import tqdm
 from importlib_metadata import entry_points
+from importlib_metadata import version
 
 import twine
-from twine import _installed
 
 args = argparse.Namespace()
 
 
 def list_dependencies_and_versions() -> List[Tuple[str, str]]:
-    return [
-        ("pkginfo", _installed.Installed(pkginfo).version),
-        ("requests", requests.__version__),
-        ("setuptools", setuptools.__version__),
-        ("requests-toolbelt", requests_toolbelt.__version__),
-        ("tqdm", tqdm.__version__),
-    ]
+    deps = (
+        "importlib_metadata",
+        "packaging",
+        "pkginfo",
+        "requests",
+        "requests-toolbelt",
+        "tqdm",
+    )
+    return [(dep, version(dep)) for dep in deps]  # type: ignore[no-untyped-call] # python/importlib_metadata#288  # noqa: E501
 
 
 def dep_versions() -> str:

commit d89585c31b28caece46534688984dc3dd422559b

Fix the "safe_name" attribute of PackageFile for backwards compatibility
Commit 0bd26af11b8f97ca97ccb9859693d7a14acf15c3 introduced a regression
that was causing some namespace packages with dots in them fail to
upload to PyPI. The reason is because
`packaging.utils.canonicalize_name()` is not equivalent to
`pkg_resources.safe_name()` as the former transforms the "." into "-",
while the later only does it for non-alphanumeric/. characters.

Closes: #743

twine/package.py
@@ -14,11 +14,11 @@
 import hashlib
 import io
 import os
+import re
 import subprocess
 from typing import Dict, NamedTuple, Optional, Sequence, Tuple, Union
 
 import importlib_metadata
-import packaging.utils
 import pkginfo
 
 from twine import exceptions
@@ -44,6 +44,14 @@ DIST_EXTENSIONS = {
 MetadataValue = Union[str, Sequence[str]]
 
 
+def _safe_name(name: str) -> str:
+    """Convert an arbitrary string to a standard distribution name.
+
+    Any runs of non-alphanumeric/. characters are replaced with a single '-'.
+    """
+    return re.sub("[^A-Za-z0-9.]+", "-", name)
+
+
 class PackageFile:
     def __init__(
         self,
@@ -59,7 +67,7 @@ class PackageFile:
         self.metadata = metadata
         self.python_version = python_version
         self.filetype = filetype
-        self.safe_name = packaging.utils.canonicalize_name(metadata.name)
+        self.safe_name = _safe_name(metadata.name)
         self.signed_filename = self.filename + ".asc"
         self.signed_basefilename = self.basefilename + ".asc"
         self.gpg_signature: Optional[Tuple[str, bytes]] = None

tests/test_package.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import string
+
 import pretend
 import pytest
 
@@ -108,6 +110,28 @@ def test_package_signed_name_is_correct():
 
     assert package.signed_basefilename == "deprecated-pypirc.asc"
     assert package.signed_filename == (filename + ".asc")
+
+
+@pytest.mark.parametrize(
+    "pkg_name,expected_name",
+    [
+        (string.ascii_letters, string.ascii_letters),
+        (string.digits, string.digits),
+        (string.punctuation, "-.-"),
+        ("mosaik.SimConfig", "mosaik.SimConfig"),
+        ("mosaik$$$$.SimConfig", "mosaik-.SimConfig"),
+    ],
+)
+def test_package_safe_name_is_correct(pkg_name, expected_name):
+    package = package_file.PackageFile(
+        filename="tests/fixtures/deprecated-pypirc",
+        comment=None,
+        metadata=pretend.stub(name=pkg_name),
+        python_version=None,
+        filetype=None,
+    )
+
+    assert package.safe_name == expected_name
 
 
 @pytest.mark.parametrize("gpg_signature", [(None), (pretend.stub())])