b7a38d9d4537755f1bc0e90771a50471039d03a5
Brian Rutledge <brian@bhrutledge.com>
2022-03-27 06:48:53 -0400

    Reconfigure output before each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import getpass
+import logging.config
 import textwrap
 
 import pytest
@@ -8,11 +9,34 @@ from twine import settings
 from twine import utils
 
 
-def pytest_configure(config):
-    # Disable color codes and wrapping during tests
+@pytest.fixture(autouse=True)
+def configure_output():
+    """
+    Disable colored output and line wrapping before each test.
+
+    Some tests (e.g. test_main.py) will end up calling (and making assertions based on)
+    twine.cli.configure_output, which overrides this configuration. This fixture should
+    prevent that leaking into subsequent tests.
+    """
     rich.reconfigure(
         no_color=True,
         width=500,
+    )
+
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                }
+            },
+            "loggers": {
+                "twine": {
+                    "handlers": ["console"],
+                },
+            },
+        }
     )
 
 
