4f09bce12e46d4232704a0c5ee459106fe0d55fc
Bruno Alla <alla.brunoo@gmail.com>
2024-10-13 13:55:33 +0100

    Rename import as to valid_classifiers

--- twine/commands/check.py
+++ twine/commands/check.py
@@ -22,7 +22,7 @@ from typing import Dict, List, Optional, Sequence, Tuple, cast
 
 import readme_renderer.rst
 from rich import print
-from trove_classifiers import classifiers as all_classifiers
+from trove_classifiers import classifiers as valid_classifiers
 
 from twine import commands
 from twine import package as package_file
@@ -113,7 +113,7 @@ def _check_file(
     # Check classifiers
     dist_classifiers = cast(Sequence[str], metadata["classifiers"])
     for classifier in dist_classifiers:
-        if classifier not in all_classifiers:
+        if classifier not in valid_classifiers:
             errors.append(
                 f"`{classifier}` is not a valid classifier"
                 f" and would prevent upload to PyPI."
