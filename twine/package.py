93b69928df2fc3d77fae2a31badd44fee60691d6
Daniele Nicolodi <daniele@grinta.net>
2024-11-26 00:19:16 +0100

    Move monkeypatching of metadata 2.0 support to a more proper place
    It was done in the support code for the wheel file format but it
    affects metadata loading from all supported distribution types. Move
    it to generic code.

--- twine/package.py
+++ twine/package.py
@@ -27,6 +27,18 @@ from twine import exceptions
 from twine import sdist
 from twine import wheel
 
+# Monkeypatch Metadata 2.0 support
+metadata._VALID_METADATA_VERSIONS = [
+    "1.0",
+    "1.1",
+    "1.2",
+    "2.0",
+    "2.1",
+    "2.2",
+    "2.3",
+    "2.4",
+]
+
 DIST_TYPES = {
     "bdist_wheel": wheel.Wheel,
     "sdist": sdist.SDist,
