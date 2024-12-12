9924a3390341c8906aa034563d76c1c47bbffee1
Daniele Nicolodi <daniele@grinta.net>
2024-12-12 22:23:23 +0100

    Tighten regular expression used to validate wheel filenames
    Drop the .dist-info file extension. Enforce that each component of the
    filename does not contain a dash: dashes are used as separators.
    
    See https://packaging.python.org/en/latest/specifications/binary-distribution-format/#file-name-convention
    
    Remove spurious grouping, while at it.

--- twine/wheel.py
+++ twine/wheel.py
@@ -20,9 +20,13 @@ from twine import distribution
 from twine import exceptions
 
 wheel_file_re = re.compile(
-    r"""^(?P<namever>(?P<name>.+?)(-(?P<ver>\d.+?))?)
-        ((-(?P<build>\d.*?))?-(?P<pyver>.+?)-(?P<abi>.+?)-(?P<plat>.+?)
-        \.whl|\.dist-info)$""",
+    r"""^(?P<name>[^-]+)-
+         (?P<version>[^-]+)
+         (:?-(?P<build>\d[^-]*))?-
+         (?P<pyver>[^-]+)-
+         (?P<abi>[^-]+)-
+         (?P<plat>[^-]+)
+         \.whl$""",
     re.VERBOSE,
 )
 
